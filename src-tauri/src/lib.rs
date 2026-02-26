50b1bed1a6152661cbf023474ff4c3e7a57e7e35
kurniadi <kurniadikkd@gmail.com>
2026-02-26 22:29:18 +0700

    feat: integrate core tauri plugins and native android permissions

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -407,6 +407,12 @@ pub fn run() {
     ];
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_biometric::init())
+        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_geolocation::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(
